fd0382ffef43aedb616e2ba5ada6d2d6fb418bfb
Germán Schnyder <gschnyder@gmail.com>
2019-09-19 14:40:33 +0200

    Ignoring id if not specified in options

--- test/index.js
+++ test/index.js
@@ -95,13 +95,12 @@ describe('#convert', () => {
         assert.strictEqual(output, result);
     });
 
-    /* IGNORE for now, difer on id
     it('should convert from 2.0.0-rc1 to 2.0.0', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '2.0.0-rc1', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0');
         assert.strictEqual(output, result);
-    });*/
+    });
 
     it('should convert from 2.0.0-rc2 to 2.0.0', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '2.0.0-rc2', 'streetlights.yml'), 'utf8');
