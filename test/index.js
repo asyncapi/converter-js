2f21c15a24821ad4c0edad8ef89a7a4c14023b59
derberg <lpgornicki@gmail.com>
2021-02-10 13:06:43 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -8,38 +8,35 @@ describe('#convert', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.0.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc1', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc1');
-        console.log('output', output)
-        console.log('switchToUnixLinebreaks(output)', switchToUnixLinebreaks(output))
-        console.log('result', result)
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
     
     it('should convert from 1.1.0 to 2.0.0-rc1', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.1.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc1', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc1');
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
     
     it('should convert from 1.2.0 to 2.0.0-rc1', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc1', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc1');
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
     
     it('should convert from 1.2.0 to 2.0.0-rc1 - stream', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'gitter-streaming.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc1', 'gitter-streaming.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc1');
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
     
     it('should convert from 1.2.0 to 2.0.0-rc1 - events', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'slack-rtm.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc1', 'slack-rtm.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc1');
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
     
     it('should convert from 1.0.0 to 2.0.0-rc2', () => {
@@ -53,63 +50,63 @@ describe('#convert', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.1.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc2', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc2');
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
     
     it('should convert from 1.2.0 to 2.0.0-rc2 - stream', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'gitter-streaming.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc2', 'gitter-streaming.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc2');
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
     
     it('should convert from 1.2.0 to 2.0.0-rc2 - events', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'slack-rtm.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc2', 'slack-rtm.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc2');
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
 
     it('should convert from 1.2.0 to 2.0.0-rc2', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc2', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc2');
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
 
     it('should convert from 1.0.0 to 2.0.0', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.0.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0');
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
 
     it('should convert from 1.1.0 to 2.0.0', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.1.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0');
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
 
     it('should convert from 1.2.0 to 2.0.0', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0');
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
 
     it('should convert from 2.0.0-rc1 to 2.0.0', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '2.0.0-rc1', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0');
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
 
     it('should convert from 2.0.0-rc2 to 2.0.0', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '2.0.0-rc2', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0');
-        assert.strictEqual(switchToUnixLinebreaks(output), result);
+        assert.strictEqual(switchToUnixLinebreaks(output), switchToUnixLinebreaks(result));
     });
 });
 
