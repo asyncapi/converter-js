1d63118d7c7af3c5014928bc2b398b77eeed6fa8
Lukasz Gornicki <lpgornicki@gmail.com>
2021-02-10 14:31:39 +0100

    test: add helper for handling windows newlines on CI (#30)

--- test/index.js
+++ test/index.js
@@ -8,104 +8,116 @@ describe('#convert', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.0.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc1', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc1');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
     
     it('should convert from 1.1.0 to 2.0.0-rc1', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.1.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc1', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc1');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
     
     it('should convert from 1.2.0 to 2.0.0-rc1', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc1', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc1');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
     
     it('should convert from 1.2.0 to 2.0.0-rc1 - stream', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'gitter-streaming.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc1', 'gitter-streaming.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc1');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
     
     it('should convert from 1.2.0 to 2.0.0-rc1 - events', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'slack-rtm.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc1', 'slack-rtm.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc1');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
     
     it('should convert from 1.0.0 to 2.0.0-rc2', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.0.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc2', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc2');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
     
     it('should convert from 1.1.0 to 2.0.0-rc2', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.1.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc2', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc2');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
     
     it('should convert from 1.2.0 to 2.0.0-rc2 - stream', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'gitter-streaming.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc2', 'gitter-streaming.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc2');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
     
     it('should convert from 1.2.0 to 2.0.0-rc2 - events', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'slack-rtm.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc2', 'slack-rtm.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc2');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
 
     it('should convert from 1.2.0 to 2.0.0-rc2', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0-rc2', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0-rc2');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
 
     it('should convert from 1.0.0 to 2.0.0', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.0.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
 
     it('should convert from 1.1.0 to 2.0.0', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.1.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
 
     it('should convert from 1.2.0 to 2.0.0', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '1.2.0', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
 
     it('should convert from 2.0.0-rc1 to 2.0.0', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '2.0.0-rc1', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
 
     it('should convert from 2.0.0-rc2 to 2.0.0', () => {
         const input = fs.readFileSync(path.resolve(__dirname, 'input', '2.0.0-rc2', 'streetlights.yml'), 'utf8');
         const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.0.0', 'streetlights.yml'), 'utf8');
         const result = convert(input, '2.0.0');
-        assert.strictEqual(output, result);
+        assertResults(output, result);
     });
 });
+
+/*
+  It is a helper required for testing on windows. It can't be solved by editor configuration and the end line setting because expected result is converted during tests.
+  We need to remove all line breaks from the string
+*/
+function removeLineBreaks(str) {
+    return str.replace(/\r?\n|\r/g, '')
+}
+
+function assertResults(output, result){
+    assert.strictEqual(removeLineBreaks(output), removeLineBreaks(result));
+}
