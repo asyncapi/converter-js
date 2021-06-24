fd258fb488ac515eb2c9438851ff937118222d72
Matatjahu <urbanczyk.maciej.95@gmail.com>
2021-06-24 22:13:08 +0200

    feat: convert from 2.0.0 to 2.1.0

--- test/index.js
+++ test/index.js
@@ -108,6 +108,13 @@ describe('#convert', () => {
         const result = convert(input, '2.0.0');
         assertResults(output, result);
     });
+
+    it('should convert from 2.0.0 to 2.1.0', () => {
+        const input = fs.readFileSync(path.resolve(__dirname, 'input', '2.0.0', 'streetlights.yml'), 'utf8');
+        const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.1.0', 'streetlights.yml'), 'utf8');
+        const result = convert(input, '2.1.0');
+        assertResults(output, result);
+    });
 });
 
 /*
