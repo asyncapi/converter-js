2394035f3bc1b23c945a11798cfef7fe4c5fe21d
Matatjahu <urbanczyk.maciej.95@gmail.com>
2022-01-05 16:09:49 +0100

    feat: support 2.3.0

--- test/index.js
+++ test/index.js
@@ -179,6 +179,20 @@ describe('#convert', () => {
         const result = convert(input, '2.2.0');
         assertResults(output, result);
     });
+
+    it('should convert from 2.1.0 to 2.3.0', () => {
+        const input = fs.readFileSync(path.resolve(__dirname, 'input', '2.1.0', 'streetlights.yml'), 'utf8');
+        const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.3.0', 'streetlights.yml'), 'utf8');
+        const result = convert(input, '2.3.0');
+        assertResults(output, result);
+    });
+
+    it('should convert from 2.2.0 to 2.3.0', () => {
+        const input = fs.readFileSync(path.resolve(__dirname, 'input', '2.2.0', 'streetlights.yml'), 'utf8');
+        const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.3.0', 'streetlights.yml'), 'utf8');
+        const result = convert(input, '2.3.0');
+        assertResults(output, result);
+    });
 });
 
 /*
