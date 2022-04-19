238334b768f5d461d07988ac546ef909a6992b0b
Matatjahu <urbanczyk.maciej.95@gmail.com>
2022-04-19 15:02:23 +0200

    feat: support 2.4.0 version of AsyncAPI

--- src/convert.ts
+++ src/convert.ts
@@ -23,6 +23,7 @@ import type { AsyncAPIDocument, ConvertVersion, ConvertOptions } from './interfa
   '2.1.0': from__2_0_0__to__2_1_0,
   '2.2.0': from__2_1_0__to__2_2_0,
   '2.3.0': from__2_2_0__to__2_3_0,
+  '2.4.0': from__2_3_0__to__2_4_0,
 };
 const conversionVersions = Object.keys(conversions);
 
@@ -212,5 +213,10 @@ function from__2_1_0__to__2_2_0(asyncapi: AsyncAPIDocument, _: ConvertOptions) {
 
 function from__2_2_0__to__2_3_0(asyncapi: AsyncAPIDocument, _: ConvertOptions) {
   asyncapi.asyncapi = '2.3.0';
+  return asyncapi;
+}
+
+function from__2_3_0__to__2_4_0(asyncapi: AsyncAPIDocument, _: ConvertOptions) {
+  asyncapi.asyncapi = '2.4.0';
   return asyncapi;
 }
--- test/convert.spec.ts
+++ test/convert.spec.ts
@@ -191,6 +191,20 @@ it('should convert from 2.2.0 to 2.3.0', () => {
     assertResults(output, result);
 });
 
+it('should convert from 2.2.0 to 2.4.0', () => {
+    const input = fs.readFileSync(path.resolve(__dirname, 'input', '2.2.0', 'streetlights.yml'), 'utf8');
+    const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.4.0', 'streetlights.yml'), 'utf8');
+    const result = convert(input, '2.4.0');
+    assertResults(output, result);
+});
+
+it('should convert from 2.3.0 to 2.4.0', () => {
+    const input = fs.readFileSync(path.resolve(__dirname, 'input', '2.3.0', 'streetlights.yml'), 'utf8');
+    const output = fs.readFileSync(path.resolve(__dirname, 'output', '2.4.0', 'streetlights.yml'), 'utf8');
+    const result = convert(input, '2.4.0');
+    assertResults(output, result);
+});
+
 it('should convert from 2.0.0 to 2.1.0 (JSON case)', () => {
     const input = fs.readFileSync(path.resolve(__dirname, 'input', '2.0.0', 'streetlights.json'), 'utf8');
     let output = fs.readFileSync(path.resolve(__dirname, 'output', '2.1.0', 'streetlights.json'), 'utf8');
