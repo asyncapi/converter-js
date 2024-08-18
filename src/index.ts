124702fd42657291c580375d5450e0a4c8ef302c
utnim2 <mintugogoi567@gmail.com>
2024-08-19 02:35:17 +0530

    fix the wrong import

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { convert } from './convert';
+export { convert, convertOpenAPI } from './convert';
 
 export type { AsyncAPIDocument, AsyncAPIConvertVersion, OpenAPIConvertVersion, ConvertOptions } from './interfaces';
