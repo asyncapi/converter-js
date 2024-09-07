8f35f7bd39d217071522083b4e493e3a75d362cc
ItshMoh <mohan.kumar.min22@itbhu.ac.in>
2024-09-07 14:35:41 +0530

    Added perspective options in from_postman_to_asyncapi function

--- src/postman-collection.ts
+++ src/postman-collection.ts
@@ -2,6 +2,7 @@ import collection from '../../collection.json';
 import { transpile } from 'postman2openapi';
 import { from_openapi_to_asyncapi } from './openapi';
 import { ConvertPostmanFunction } from 'interfaces';
+import { PostmanToAsyncAPIOptions } from 'interfaces';
 
 export const converters: Record<string, ConvertPostmanFunction > = {
     '3.0.0': from_postman_to_asyncapi
@@ -9,9 +10,10 @@ export const converters: Record<string, ConvertPostmanFunction > = {
 
 const openapi = transpile(collection);
 
-function from_postman_to_asyncapi(postman: any) {
+function from_postman_to_asyncapi(postman: any ,options:PostmanToAsyncAPIOptions ={}) {
+    const perspective = options.perspective;
     const openapi = transpile(postman);
-    const asyncapi = from_openapi_to_asyncapi(openapi);
+    const asyncapi = from_openapi_to_asyncapi(openapi , {perspective:perspective});
     return asyncapi;
 }
 
