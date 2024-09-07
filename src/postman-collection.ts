56c8f4fd7e8599d71b9724f0c21cee0c827f7e18
ItshMoh <mohan.kumar.min22@itbhu.ac.in>
2024-09-08 01:33:10 +0530

    finalllyyyy

--- src/postman-collection.ts
+++ src/postman-collection.ts
@@ -6,14 +6,9 @@ export const converters: Record<string, ConvertPostmanFunction > = {
     '3.0.0': from_postman_to_asyncapi
 }
 
-const openapi = transpile(collection);
-
 function from_postman_to_asyncapi(postman: any ,options:PostmanToAsyncAPIOptions ={}) {
     const perspective = options.perspective;
     const openapi = transpile(postman);
     const asyncapi = from_openapi_to_asyncapi(openapi , {perspective:perspective});
     return asyncapi;
 }
-
-const output = from_postman_to_asyncapi(collection);
-
