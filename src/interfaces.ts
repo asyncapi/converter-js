2f585275dee4ce171a612a775b7c63bed42272ba
Ashish Padhy <ashishpadhy1729@gmail.com>
2026-01-31 16:27:23 +0530

    feat: add 3.1.0 to version interface (#309)
    Co-authored-by: Shurtu-gal <ashishpadhy1729@gmail.com>

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,7 +3,7 @@
  */
 export type AsyncAPIDocument = { asyncapi: string } & Record<string, any>;
 export type OpenAPIDocument = { openapi: string } & Record<string, any>;
-export type AsyncAPIConvertVersion = '1.1.0' | '1.2.0' | '2.0.0-rc1' | '2.0.0-rc2' | '2.0.0' | '2.1.0' | '2.2.0' | '2.3.0' | '2.4.0' | '2.5.0' | '2.6.0' | '3.0.0';
+export type AsyncAPIConvertVersion = '1.1.0' | '1.2.0' | '2.0.0-rc1' | '2.0.0-rc2' | '2.0.0' | '2.1.0' | '2.2.0' | '2.3.0' | '2.4.0' | '2.5.0' | '2.6.0' | '3.0.0' | '3.1.0';
 
 export type OpenAPIConvertVersion = '3.0.0';
 export type ConvertV2ToV3Options = {
